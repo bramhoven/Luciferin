b24b98d74ae9193ae1f84246de475fceb78b556d
Bram Hoven <bram.hoven@hotmail.com>
2023-04-02 12:51:24 +0200

    Add bank name validation messages

--- Luciferin.Website/Controllers/ConfigurationController.cs
+++ Luciferin.Website/Controllers/ConfigurationController.cs
@@ -7,83 +7,95 @@ using Luciferin.Website.Models.Configuration;
 using Luciferin.Website.Models.Configuration.FormModels;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Luciferin.Website.Controllers
+namespace Luciferin.Website.Controllers;
+
+[Route("configuration")]
+public class ConfigurationController : Controller
 {
-    [Route("configuration")]
-    public class ConfigurationController : Controller
+    #region Constructors
+
+    public ConfigurationController(INordigenManager nordigenManager, IImportManager importManager)
     {
-        #region Fields
-
-        private readonly IImportManager _importManager;
-
-        private readonly INordigenManager _nordigenManager;
-
-        private const string ForwardedPortHeader = "X-Forwarded-Port";
-
-        #endregion
-
-        #region Constructors
-
-        public ConfigurationController(INordigenManager nordigenManager, IImportManager importManager)
-        {
-            _nordigenManager = nordigenManager;
-            _importManager = importManager;
-        }
-
-        #endregion
-
-        #region Methods
-
-        [HttpPost]
-        [Route("/bank")]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> AddBank(ConfigurationAddBankFormModel formModel)
-        {
-            if (string.IsNullOrWhiteSpace(formModel.InstitutionId))
-                return RedirectToAction("Index", "Configuration");
-
-            var forwardHeaderPort = Request.Headers.ContainsKey(ForwardedPortHeader) ? (int?)int.Parse(Request.Headers[ForwardedPortHeader]) : null;
-            var redirectUrl = new UriBuilder(Request.Host.Host)
-            {
-                Scheme = Uri.UriSchemeHttps,
-                Path = "/configuration",
-                Port = forwardHeaderPort ?? Request.Host.Port ?? 80
-            }.ToString();
-            var requisition = await _importManager.AddNewBank(formModel.InstitutionId, formModel.BankName, redirectUrl);
-
-            return Redirect(requisition.Link);
-        }
-
-        [HttpPost]
-        [Route("/bank/delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteBank(string requisitionId)
-        {
-            if (string.IsNullOrWhiteSpace(requisitionId))
-                return RedirectToAction("Index", "Configuration");
-
-            await _importManager.DeleteBank(requisitionId);
-
-            return RedirectToAction("Index", "Configuration");
-        }
-        
-        [HttpGet]
-        public async Task<ActionResult> Index()
-        {
-            var model = new ConfigurationIndexPageModel
-            {
-                AddBankFormModel = new ConfigurationAddBankFormModel
-                {
-                    Institutions = await _nordigenManager.GetInstitutions("NL")
-                },
-                RequisitionList = new RequisitionList(await _nordigenManager.GetRequisitions())
-                {
-                    Deletable = true
-                }
-            };
-            return View(model);
-        }
-
-        #endregion
+        _nordigenManager = nordigenManager;
+        _importManager = importManager;
     }
+
+    #endregion
+
+    #region Fields
+
+    private readonly IImportManager _importManager;
+
+    private readonly INordigenManager _nordigenManager;
+
+    private const string ForwardedPortHeader = "X-Forwarded-Port";
+
+    #endregion
+
+    #region Methods
+
+    [HttpPost]
+    [Route("/bank")]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> AddBank(ConfigurationIndexPageModel pageModel)
+    {
+        var formModel = pageModel.AddBankFormModel;
+        if (!ModelState.IsValid)
+        {
+            var model = await GetDefaultModel();
+            model.AddBankFormModel.InstitutionId = formModel.InstitutionId;
+            model.AddBankFormModel.BankName = formModel.BankName;
+            return View(nameof(Index), model);
+        }
+
+        var forwardHeaderPort = Request.Headers.ContainsKey(ForwardedPortHeader)
+            ? (int?)int.Parse(Request.Headers[ForwardedPortHeader])
+            : null;
+        var redirectUrl = new UriBuilder(Request.Host.Host)
+        {
+            Scheme = Uri.UriSchemeHttps,
+            Path = "/configuration",
+            Port = forwardHeaderPort ?? Request.Host.Port ?? 80
+        }.ToString();
+        var requisition = await _importManager.AddNewBank(formModel.InstitutionId, formModel.BankName, redirectUrl);
+
+        return Redirect(requisition.Link);
+    }
+
+    [HttpPost]
+    [Route("/bank/delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> DeleteBank(string requisitionId)
+    {
+        if (string.IsNullOrWhiteSpace(requisitionId))
+            return RedirectToAction("Index", "Configuration");
+
+        await _importManager.DeleteBank(requisitionId);
+
+        return RedirectToAction("Index", "Configuration");
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> Index()
+    {
+        return View(await GetDefaultModel());
+    }
+
+    private async Task<ConfigurationIndexPageModel> GetDefaultModel()
+    {
+        var model = new ConfigurationIndexPageModel
+        {
+            AddBankFormModel = new ConfigurationAddBankFormModel
+            {
+                Institutions = await _nordigenManager.GetInstitutions("NL")
+            },
+            RequisitionList = new RequisitionList(await _nordigenManager.GetRequisitions())
+            {
+                Deletable = true
+            }
+        };
+        return model;
+    }
+
+    #endregion
 }
--- Luciferin.Website/Models/Configuration/FormModels/ConfigurationAddBankFormModel.cs
+++ Luciferin.Website/Models/Configuration/FormModels/ConfigurationAddBankFormModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Luciferin.BusinessLayer.Nordigen.Models;
 
 namespace Luciferin.Website.Models.Configuration.FormModels
@@ -6,9 +7,11 @@ namespace Luciferin.Website.Models.Configuration.FormModels
     public class ConfigurationAddBankFormModel
     {
         #region Properties
-
+        
+        [Required(ErrorMessage = "Bank name is required")]
         public string BankName { get; set; }
 
+        [Required(ErrorMessage = "Institution is required")]
         public string InstitutionId { get; set; }
 
         public ICollection<Institution> Institutions { get; set; }
