4dabe81628814b95c0b27285b7548e4edec6cb44
Bram Hoven <bram.hoven@hotmail.com>
2023-04-02 12:51:22 +0200

    Add null check

--- Luciferin.BusinessLayer/Firefly/Models/FireflyAccount.cs
+++ Luciferin.BusinessLayer/Firefly/Models/FireflyAccount.cs
@@ -32,6 +32,9 @@ namespace Luciferin.BusinessLayer.Firefly.Models
         /// <inheritdoc />
         public override bool Equals(object? obj)
         {
+            if (obj == null)
+                return false;
+            
             if (!(obj is FireflyAccount fireflyAccount))
                 return false;
 
